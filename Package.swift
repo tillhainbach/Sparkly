fea819c54f671e6fa548b7a373d085d14b64eeac
Till Hainbach <Till.Hainbach@posteo.de>
2021-06-03 16:37:22 +0200

    chore(sparkly,manifest): update targets and deps in manifest

--- Package.swift
+++ Package.swift
@@ -14,20 +14,29 @@ let package = Package(
     .library(
       name: "SUUpdaterClient",
       targets: ["SUUpdaterClient"]),
+    .library(
+      name: "SUUpdaterClientLive",
+      targets: ["SUUpdaterClientLive"]),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
+    .package(path: "./Sparkle")
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "SparklyCommands",
-      dependencies: []),
+      dependencies: []
+    ),
     .target(
       name: "SUUpdaterClient",
-      dependencies: []),
+      dependencies: []
+    ),
+    .target(
+      name: "SUUpdaterClientLive",
+      dependencies: ["Sparkle"]
+    ),
     .testTarget(
       name: "SUUpdaterClientTests",
       dependencies: []),
