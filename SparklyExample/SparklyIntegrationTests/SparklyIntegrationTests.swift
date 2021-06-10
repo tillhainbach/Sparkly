d4e98c40ce79b0229999cd350aaa1075a970de99
Till Hainbach <Till.Hainbach@posteo.de>
2021-06-10 17:26:00 +0200

    test(sparkly,live): add happy-path test

--- SparklyExample/SparklyIntegrationTests/SparklyIntegrationTests.swift
+++ SparklyExample/SparklyIntegrationTests/SparklyIntegrationTests.swift
@@ -42,5 +42,39 @@ class SparklyIntegrationTests: XCTestCase {
     client.send(.checkForUpdates)
   }
 
-}
+  func test_LiveSparkle_HappyPathFlow() throws {
+    let testBundle = Bundle(for: type(of: self))
 
+    let client = SUUpdaterClient.live(hostBundle: testBundle, applicationBundle: testBundle)
+
+    var canCheckForUpdates: [Bool] = [false]
+    var receivedEvents: [SUUpdaterClient.UpdaterEvents] = []
+
+    client.updaterEventPublisher
+      .sink { event in
+        receivedEvents.append(event)
+        switch event {
+        case .canCheckForUpdates(let newCanCheckForUpdates):
+          canCheckForUpdates.append(newCanCheckForUpdates)
+          break
+
+        default:
+          XCTFail("Wrong event was sent")
+        }
+      }
+      .store(in: &cancellables)
+
+    XCTAssertEqual(canCheckForUpdates, [false])
+
+    // start updater
+    client.send(.startUpdater)
+
+    XCTAssertEqual(canCheckForUpdates, [false, true])
+    XCTAssertEqual(receivedEvents, [.canCheckForUpdates(true)])
+
+    client.send(.checkForUpdates)
+
+    wait(for: [XCTestExpectation(description: "Some description")], timeout: 2)
+  }
+
+}
