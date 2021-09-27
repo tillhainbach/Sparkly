165a9ab87a316fa7909f669e2810b81306c976d1
Till Hainbach <Till.Hainbach@posteo.de>
2021-09-27 09:30:34 +0200

    build(sparkly): pin sparkle-version to a specific beta-version
    Sparkle's and XPC's version must match exactly.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
   name: "Sparkly",
   platforms: [.macOS(.v11)],
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(
       name: "Sparkly",
       targets: ["Sparkly"]
@@ -26,12 +25,15 @@ let package = Package(
     ),
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    .package(name: "Sparkle", url: "https://github.com/sparkle-project/Sparkle", .branch("2.x"))
+    .package(
+      name: "Sparkle",
+      url: "https://github.com/sparkle-project/Sparkle",
+      .exactItem(
+        Version(2, 0, 0, prereleaseIdentifiers: ["beta", "2"], buildMetadataIdentifiers: [])
+      )
+    )
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "Sparkly",
       dependencies: ["SparklyClientLive", "SparklyCommands"]
