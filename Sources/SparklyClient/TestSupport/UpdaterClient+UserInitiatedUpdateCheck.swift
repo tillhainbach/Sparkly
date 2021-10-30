170f7ed5ae89e703f0ed8e7ba34bc6a979cdb5e4
Till Hainbach <Till.Hainbach@posteo.de>
2021-10-31 01:28:05 +0200

    refactor(sparkly-client): use interface class for mock

--- Sources/SparklyClient/TestSupport/UpdaterClient+UserInitiatedUpdateCheck.swift
+++ Sources/SparklyClient/TestSupport/UpdaterClient+UserInitiatedUpdateCheck.swift
@@ -13,26 +13,23 @@ import Foundation
 
 extension UpdaterClient {
   public static func mockUserInitiatedUpdateCheck(scheduler: TestSchedulerOf<DispatchQueue>) -> Self {
-    let mockUpdater = PassthroughSubject<UpdaterClient.Action, Never>()
-    let publisher = PassthroughSubject<UpdaterClient.Event, Never>()
-    var cancellables: Set<AnyCancellable> = []
 
     var currentState: UpdateCheckState!
 
-    mockUpdater
-      .sink { action in
+    let interface = Interface()
+    interface.handleAction = { action in
         switch action {
         case .checkForUpdates:
-          publisher.send(.canCheckForUpdates(false))
+          interface.send(.canCheckForUpdates(false))
           currentState = .checking
-          publisher.send(.updateCheck(currentState))
+          interface.send(.updateCheck(currentState))
           scheduler.schedule {
             currentState = .found(.mock, state: .mock)
-            publisher.send(.updateCheck(currentState))
+            interface.send(.updateCheck(currentState))
           }
 
         case .startUpdater:
-          publisher.send(.canCheckForUpdates(true))
+          interface.send(.canCheckForUpdates(true))
 
         case .setPermission:
           break
@@ -41,51 +38,44 @@ extension UpdaterClient {
           break
 
         case .cancel:
-          publisher.send(.dismissUpdateInstallation)
-          publisher.send(.canCheckForUpdates(true))
+          interface.send(.dismissUpdateInstallation)
+          interface.send(.canCheckForUpdates(true))
 
         case .reply(let response):
           switch response {
           case .skip:
-            publisher.send(.dismissUpdateInstallation)
-            mockUpdater.send(.cancel)
+            interface.send(.dismissUpdateInstallation)
+            interface.actionSubject.send(.cancel)
 
           case .install:
             if case .found(_, _) = currentState {
-              publisher.send(.updateCheck(.downloading(total: 4, completed: 0)))
+              interface.send(.updateCheck(.downloading(total: 4, completed: 0)))
               scheduler.scheduleSequentially(
-                { publisher.send(.updateCheck(.downloading(total: 4, completed: 3))) },
-                { publisher.send(.updateCheck(.downloading(total: 4, completed: 4))) },
-                { publisher.send(.updateCheck(.extracting(completed: 0.0))) },
-                { publisher.send(.updateCheck(.extracting(completed: 1.0))) },
-                { publisher.send(.updateCheck(.installing)) },
+                { interface.send(.updateCheck(.downloading(total: 4, completed: 3))) },
+                { interface.send(.updateCheck(.downloading(total: 4, completed: 4))) },
+                { interface.send(.updateCheck(.extracting(completed: 0.0))) },
+                { interface.send(.updateCheck(.extracting(completed: 1.0))) },
+                { interface.send(.updateCheck(.installing)) },
                 {
                   currentState = .readyToRelaunch
-                  publisher.send(.updateCheck(.readyToRelaunch))
+                  interface.send(.updateCheck(.readyToRelaunch))
                 }
               )
             }
 
             if case .readyToRelaunch = currentState {
-              publisher.send(.dismissUpdateInstallation)
+              interface.send(.dismissUpdateInstallation)
             }
 
           case .dismiss:
-            publisher.send(.dismissUpdateInstallation)
-            mockUpdater.send(.cancel)
+            interface.send(.dismissUpdateInstallation)
+            interface.actionSubject.send(.cancel)
 
           }
         }
-
       }
-      .store(in: &cancellables)
 
-    return .init(
-      send: mockUpdater.send(_:),
-      updaterEventPublisher: publisher
-        .handleEvents(receiveCancel: { cancellables.removeAll() })
-        .eraseToAnyPublisher()
-    )
+    return interface.client
   }
 }
 
